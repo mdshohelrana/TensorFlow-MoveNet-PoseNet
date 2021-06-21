9c26ef05a048ef3da6b1de702faa96ab3af985aa
Shohel <shohel.rana@mymedicalhub.com>
2021-06-21 22:43:28 +0600

    added tailwind css

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import './App.css';
+// eslint-disable-next-line
+import * as tfJsCore from '@tensorflow/tfjs-core';
+// eslint-disable-next-line
+import * as tfJsConverter from '@tensorflow/tfjs-converter';
+// eslint-disable-next-line
+import * as tfJsBackendWebgl from '@tensorflow/tfjs-backend-webgl';
 
 import {useEffect, useState} from "react";
-import * as tfJsCore from '@tensorflow/tfjs-core';
-import * as tfJsConverter from '@tensorflow/tfjs-converter';
-import * as tfJsBackendWebgl from '@tensorflow/tfjs-backend-webgl';
 import * as poseDetection from '@tensorflow-models/pose-detection';
 import model1 from "./assets/img/model1.jpg";
 
@@ -22,13 +25,15 @@ function App() {
             setKeyPoints(JSON.stringify(poses[0].keypoints));
         }
 
+        // eslint-disable-next-line
+        // noinspection JSIgnoredPromiseFromCall
         fetchData();
     }, []); // Or [] if effect doesn't need props or state
 
     return (
         <div className="App">
             <header className="App-header">
-                <img id="video"
+                <img alt="exercise" id="video"
                      crossOrigin='anonymous'
                      src={model1}/>
                 {keyPoints}
